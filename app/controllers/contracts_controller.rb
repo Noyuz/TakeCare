a331f6d55c5962d91ab55bf0709b288fc1d47ce1
Kevin Abergel <k.abergel91@gmail.com>
2019-05-21 14:11:51 +0200

    Master OK 21-05-19

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,9 +1,10 @@
 class ContractsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
 
   def show
     @contract = Contract.find(params[:id])
-  skip_before_action :authenticate_user!, only: :index
-    
+  end
+
   def index
     @contracts = Contract.all
   end
@@ -12,6 +13,5 @@ class ContractsController < ApplicationController
 
   def contract_params
     params.require(:contract).permit(:nickname, :description, :price, :rank)
-
   end
 end
