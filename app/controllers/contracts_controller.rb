55908c9b19d8f6089d852ca72857dfe87a6acf57
moral nicolas <mrwallp@gmail.com>
2019-05-23 18:25:33 +0200

    indexPath

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -10,7 +10,7 @@ class ContractsController < ApplicationController
     @contract = Contract.new(contract_params)
     @contract.user = current_user
     if @contract.save
-      redirect_to @contract, notice: "Wellcome '#{@contract.rank}'"
+      redirect_to contracts_path, notice: "Wellcome '#{@contract.nickname}'"
     else
       render :new
     end
