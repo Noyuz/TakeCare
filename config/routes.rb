0d6ece4840db61ae057ae569ea5aae79f60201af
Anne Laure Voisin <annelaurevoisin@gmail.com>
2019-05-23 14:23:14 +0200

    user story one ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :contracts do
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:new, :create]
   end
+
+  resources :bookings, only: [:index, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
